b2cd5c25584d67c078027aa3cf721bf116fc7939
laabk <laabkdavid@gmail.com>
2023-10-03 06:05:11 -0700

    4-print_hexa.py

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
 for digts in range(0, 99):
-    dec_digts = digts
-    hexa_digts = hex(digts)
-    print(f"{dec_digts} = {hexa_digts}")
+        print("{0:d} = 0x{0:x}".format(digts))
