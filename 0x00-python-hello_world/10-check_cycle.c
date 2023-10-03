314d071fc647dff23e50c54f2cb88a79bdb207c1
laabk <laabkdavid@gmail.com>
2023-10-03 11:53:02 -0700

    10-check_cycle.c

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -4,22 +4,36 @@
  * check_cycle - checks to ensure linked list has a cycle
  * @list: the linked list that is going to be check
  * Return: if 1 list has cycle, if 0 it no any cycle
- *
  */
 
 int check_cycle(listint_t *list)
 {
-		listint_t *rates = list, *spd = list;
+	listint_t *bck = list, *bcks = lsit;
 
-		if (!list)
-		return (0);
+	while (list && bck && bck->next)
+	{
+		list = list->next;
+		bck = bck->next->next;
 
-		while (rates && sped && sped->next)
+		if (list == bck)
 		{
-		rates = rates->next;
-		sped = sped->next->next;
-		if (rates == sped)
-		return (1);
+			list = bcks;
+			bcks =  bck;
+			while (1)
+			{
+				bck = bcks;
+				while (bck->next != list && bck->next != bcks)
+				{
+					bck = bck->next;
+				}
+				if (bck->next == list)
+					break;
+
+				list = list->next;
+			}
+			return (1);
 		}
-		return (0);
+	}
+
+	return (0);
 }
