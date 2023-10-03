63392dc8444eaae41cf96ddf1c379e57bc7b09c4
laabk <laabkdavid@gmail.com>
2023-10-03 11:30:29 -0700

    100-print_tebahpla.py

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-for the_cha in reversed(range(97, 123)):
-    print("{:c}".format(the_cha if (the_cha % 2 == 0) else (the_cha - 32)), end='')
+for char in reversed(range(97, 123)):
+    print("{:c}".format(char if (char % 2 == 0) else (char - 32)), end='')
