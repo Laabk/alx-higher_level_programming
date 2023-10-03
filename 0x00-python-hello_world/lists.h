d9397e22b057b82486f8f8492d399862ccefb148
laabk <laabkdavid@gmail.com>
2023-10-03 11:57:49 -0700

    lists.h

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -12,13 +12,13 @@
  */
 typedef struct listint_s
 {
-		int n;
-		struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
 
-void free_listint(listint_t *head);
 size_t print_listint(const listint_t *h);
-listint_t *add_nodeint(listint_t **head, const int n);
 int check_cycle(listint_t *list);
+listint_t *add_nodeint(listint_t **head, const int n);
+void free_listint(listint_t *head);
 
 #endif
