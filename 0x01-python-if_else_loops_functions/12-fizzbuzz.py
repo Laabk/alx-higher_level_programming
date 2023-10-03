a863875abaad4f7d2f4e79d9272530a52505c475
laabk <laabkdavid@gmail.com>
2023-10-03 07:38:35 -0700

    12-fizzbuzz.py

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for ite in range(1, 101):
+        if ite % 3 == 0 and ite % 5 == 0:
+            print("FizzBuzz", end='')
+        elif ite % 3 == 0:
+            print("Fizz", end='')
+        elif ite % 5 == 0:
+            print("Buzz", end='')
+        else:
+            print(ite, end='')
+
+        print(" ", end='')
