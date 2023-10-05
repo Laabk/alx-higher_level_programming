c9ead9527c0fa0b5ce447ea0a7c86938da741ad5
laabk <laabkdavid@gmail.com>
2023-10-05 06:41:04 -0700

    4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import hidden_4
-    for ite in dir(hidden_4):
+    items = dir(hidden_4)
+    for ite in items:
         if ite[:2] != '_':
             print(ite)
