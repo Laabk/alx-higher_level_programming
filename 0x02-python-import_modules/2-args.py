351423be61f178a811b47f009538f18f54a6c7f3
laabk <laabkdavid@gmail.com>
2023-10-05 05:46:25 -0700

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,6 +3,6 @@ if __name__ == "__main__":
     import sys
     arguco = len(sys.argv) - 1
     if (arguco == 0):
-        print(arguco, "arguments.")
+        print('{} arguments'.format(arguco))
     elif (arguco == 1):
         print("{} argument:".format(arguco))
