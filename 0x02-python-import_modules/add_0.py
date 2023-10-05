1404117537f04bb6a0e46b392b899c826ec4bc9b
laabk <laabkdavid@gmail.com>
2023-10-05 05:06:39 -0700

    add_0.py

---
+++ 0x02-python-import_modules/add_0.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def add(b, a):
+    """the addition function to b imported
+    the arguments involved:
+        a: first integer passed
+        b: second integer passwed
+    return: a + b
+    """
+    return (b + a)
