7862baef1f62020cb51a2ccbf92a3d2b83a7f5cf
laabk <laabkdavid@gmail.com>
2023-10-05 05:10:30 -0700

    calculator_1.py

---
+++ 0x02-python-import_modules/calculator_1.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+def add(a, b):
+    """the addition function to b imported
+    the arguments involved:
+        a: first integer passed
+        b: second integer passed
+
+    Returns: a + b
+    """
+    return (a + b)
+
+def sub(a, b):
+    return (a - b)
+
+def mul(a, b):
+    return (a * b)
+
+def div(a, b):
+    return int(a / b)
+
