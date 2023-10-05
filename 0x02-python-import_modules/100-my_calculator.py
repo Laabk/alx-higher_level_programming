46cbc96b710e9b8ef22f7db9e1ee3aa3d6539430
laabk <laabkdavid@gmail.com>
2023-10-05 07:10:35 -0700

    100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -7,10 +7,8 @@ if __name__ == "__main__":
     if argmn != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
+
     oprt = sys.argv[2]
-     if oprt != '*' and oprt != '+' and op != '-' and op != '/':
+     if oprt != '*' and oprt != '+' and oprt != '-' and oprt != '/':
         print("Unknown operator. Available operators: *, +, - and /")
         sys.exit(1)
-    from calculator_1 import sub, mul, div, add
-    a = int(sys.argv[1])
-    b = int(sys.argv[3])
