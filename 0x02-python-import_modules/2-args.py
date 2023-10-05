d175eda8b02d7a328ff88eae9a4b6764cac0e22a
laabk <laabkdavid@gmail.com>
2023-10-05 06:28:46 -0700

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,9 +3,9 @@ if __name__ == "__main__":
     import sys
     agu = len(sys.argv) - 1
     if agu == 0:
-        print("{} arguments:".format(agu))
+        print('0 arguments')
     elif agu == 1:
-        print("{} argument:".format(agu))
+        print('1 arguments')
     else:
         print("{} arguments:".format(agu))
     for ite in range(agu):
