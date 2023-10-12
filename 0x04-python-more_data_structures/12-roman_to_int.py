86a2d9d9726ceb275f2922d04d17f8d84b51a30a
laabk <laabkdavid@gmail.com>
2023-10-12 03:56:40 -0700

    12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -6,5 +6,5 @@ def roman_to_int(roman_string):
     the_digt = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
     for a_rom in reversed(roman_string):
         the_arabic = the_digt[a_rom]
-        rsut = resut + the_arabic if resut < the_arabic * 5 else -the_arabic
+        rsut = resut + the_arabic if resut < the_arabic * 5 else - the_arabic
     return (resut)
