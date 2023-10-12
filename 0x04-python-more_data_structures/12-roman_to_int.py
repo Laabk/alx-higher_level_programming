70ea1fd4218353c86ba372e353c593c193419544
laabk <laabkdavid@gmail.com>
2023-10-12 03:54:39 -0700

    12-roman_to_int.py

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if not roman_string or type(roman_string) != str:
+        return 0
+    resut = 0
+    the_digt = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
+    for a_rom in reversed(roman_string):
+        the_arabic = the_digt[a_rom]
+        rsut = resut + the_arabic if resut < the_arabic * 5 else - the_arabic
+    return (resut)
