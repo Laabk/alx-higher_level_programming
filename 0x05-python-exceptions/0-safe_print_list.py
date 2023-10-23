4795ec3f7c249d7d171dc212a9e652fe8976636c
laabk <laabkdavid@gmail.com>
2023-10-23 05:34:54 -0700

    0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     numbs = 0
+
     try:
         for d in range(x):
             print("{}".format(my_list[d]), end='')
@@ -8,5 +9,6 @@ def safe_print_list(my_list=[], x=0):
             break
         else:
             numbs = numbs + 1
+
     print()
     return (numbs)
