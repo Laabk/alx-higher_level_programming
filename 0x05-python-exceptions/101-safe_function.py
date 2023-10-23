2a54a564ce764a6142861ace4109c022ec5a934b
laabk <laabkdavid@gmail.com>
2023-10-23 11:06:24 -0700

    101-safe_function.py

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import sys
+
+def safe_function(fct, *args):
+    try:
+        return fct(*args)
+    except Exception as eror:
+        print("Exception: {}".format(eror), file=sys.stderr)
+        return None
