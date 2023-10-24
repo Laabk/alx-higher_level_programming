faa4d130d5d45646168728d95f89f31442b4313e
laabk <laabkdavid@gmail.com>
2023-10-24 11:31:19 -0700

    101-square.py

---
+++ 0x06-python-classes/101-square.py
@@ -0,0 +1,52 @@
+class Square:
+    """defines a class square."""
+
+    def __init__(self, size=0, position=(0, 0)):
+        """Initialize a the  Square.
+        Args:
+            size (int) & position (tuple)
+        """
+
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        """takes size of square."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @property
+    def position(self):
+        """takes position of the square."""
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        if not isinstance(value, tuple) or len(value) != 2:
+            raise TypeError("position should be tuple of two positive integers")
+        if not all(isinstance(v, int) and v >= 0 for v in value):
+            raise TypeError("position should be tuple of two positive integers")
+        self.__position = value
+
+    def area(self):
+        """finding area of the square."""
+        return self.__size ** 2
+
+    def my_print(self):
+        """Printing out the square with symbol #."""
+        if self.__size == 0:
+            print()
+            return
+        for d in range(self.__position[1]):
+            print()
+        for d in range(self.__size):
+            print(" " * self.__position[0], end="")
+            print("#" * self.__size)
