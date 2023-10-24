789ee3f98ff7c402c75b6822ad91ee913043059c
laabk <laabkdavid@gmail.com>
2023-10-24 05:46:17 -0700

    4-square.py

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
-"defines a square by its size"
+"""defines a square by its size"""
 
 
 class Square:
+    "square by its size"
     def __init__(self, size=0):
+
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
