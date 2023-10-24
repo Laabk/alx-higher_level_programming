83c55706f580e9575555190406e9604c68bfd4d5
laabk <laabkdavid@gmail.com>
2023-10-24 05:30:09 -0700

    2-square.py

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
+"""defines a class square"""
+
 class Square:
-    """defines a class square"""
+    """the class square"""
 
     def __init__(self, size=0):
         """initialising the class to store
