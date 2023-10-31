6584adb03157ea5634ee52526968081f01745518
laabk <laabkdavid@gmail.com>
2023-10-31 16:13:09 -0700

    python

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This func defines a locked class involved"""
+
+
+class LockedClass:
+        """
+        allows the user to instatiation an attribute called first_name
+        """
+        __slots__ = ["first_name"]
