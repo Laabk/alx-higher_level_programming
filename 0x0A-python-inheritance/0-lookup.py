8cbdb631086b3026f4378c01d722d72f3e243262
laabk <laabkdavid@gmail.com>
2023-11-06 08:26:06 -0800

    README.md

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+    """ a module which returns the list of attributes
+        and methods of an object
+    """
+
+
+def lookup(obj):
+    """
+        returns the attributes list and methods of the module
+    """
+    return dir(obj)
