8c325f570951062d9d67da8e05207fa68e9b9f2b
laabk <laabkdavid@gmail.com>
2023-11-07 08:26:21 -0800

    8-class_to_json.py

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+A module function that returns a description of dictionary
+"""
+
+
+def class_to_json(obj):
+    """
+    this function returns the dictionary script of a simple data
+    """
+    return obj.__dict__
