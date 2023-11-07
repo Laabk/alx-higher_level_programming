f6c6fb3f260b6d6a0608b8db37d3549e2e417366
laabk <laabkdavid@gmail.com>
2023-11-07 08:59:57 -0800

    100-append_after.py

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+this is module function which showes a text file insertion
+"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """
+    this function inserts a text after a line containing a string in a file
+    """
+    th_text = ""
+    with open(filename) as s:
+        for line in s:
+            th_text += line
+            if search_string in line:
+                text += new_string
+    with open(filename, "w") as i:
+        i.write(text)
