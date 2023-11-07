7b4aa4b54c6c9c5b8c963b68173481ccb72f6e06
laabk <laabkdavid@gmail.com>
2023-11-07 06:56:06 -0800

    3-to_json_string.py

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+this is module of funct that represent string of JSON the function
+"""
+import json
+
+
+def to_json_string(my_obj):
+    """this returns string of JSON function"""
+    return json.dumps(my_obj)
