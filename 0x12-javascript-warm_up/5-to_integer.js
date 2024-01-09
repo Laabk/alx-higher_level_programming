3af784295ea103fc15afc583f04bbe5d713d8b78
laabk <laabkdavid@gmail.com>
2024-01-09 06:16:22 -0800

    javabasics

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,9 +1,8 @@
-
-
 #!/usr/bin/node
 
-if (isNaN(parseInt(process.argv[2])) === true) {
+const mynub = parseInt(process.argv[2]);
+if (Number.isNaN(mynub)) {
   console.log('Not a number');
 } else {
-  console.log('My number: ' + parseInt(process.argv[2]));
+  console.log('My number: ' + mynub);
 }
