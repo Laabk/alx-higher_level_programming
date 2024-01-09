25bcfd0cc395c6231932f212d4d69c71a543c264
laabk <laabkdavid@gmail.com>
2024-01-09 05:30:16 -0800

    1-multi_languages.js

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
 
-const my_var = 'C is fun\nPython is cool\nJavaScript is amazing';
-console.log(my_var);
+const myvari = 'C is fun\nPython is cool\nJavaScript is amazing';
+console.log(myvari);
