ea91b74ac4e265163b01dc78e1ece35a4df47792
laabk <laabkdavid@gmail.com>
2024-01-14 09:46:49 -0800

    0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,2 +1,10 @@
--- this lists all databases of your MySQL server.
-SHOW DATABASES;
+#!/usr/bin/python3
+""" this script lists all states from the database hbtn_0e_"""
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id")
+    [print(state) for state in cur.fetchall() if state[1][0] == "N"]
