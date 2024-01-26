7596ea1215bcff6db3d66b1cf6c037dd893d1c8b
laabk <laabkdavid@gmail.com>
2024-01-25 17:09:05 -0800

    pynetwrk

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# A bash script that sends a request to a URL passed as an argument
+# A script sends a request to a URL passed as an argument
 curl -s -o /dev/null -w "%{http_code}" "$1"
--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# bash script that sends JSON POST request to URL passed as the first a
+# A script sends JSON POST request to URL passed as the first arg
 curl -sH "Content-Type: application/json" -d "$(cat "$2")" "$1"
