50ab81c5c3a5c5509b1fb90bb23ae9f033b8da5a
laabk <laabkdavid@gmail.com>
2024-01-25 17:00:55 -0800

    pynetwrk

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# script that sends JSON POST request to URL passed as the first arg
-# and displays the body of the response.
+# bash script that sends JSON POST request to URL passed as the first a
 curl -sH "Content-Type: application/json" -d "$(cat "$2")" "$1"
--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# A bash script thats send a GET request to the URL using curl,
-#and display the body of the response
+# A bash script thats send a GET request to the URL using crul
 curl -sH "X-School-User-Id: 98" "$1"
